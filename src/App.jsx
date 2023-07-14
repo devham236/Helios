45cf4f9f469fd41d1dac98fdc90ecfc695c4dbc7
devham236 <90037750+devham236@users.noreply.github.com>
2023-07-14 09:47:41 +0200

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,8 @@ import MainContent from './Components/MainContent'
 
 const App = () => {
   return (
-    <>
-      {/* <Sidebar/>
-      <MainContent/> */}
-      <MainContent/>
-    </>
+    <MainContent/>
   )
 }
 
 export default App
-
-// https://api.openweathermap.org/data/2.5/weather?q=hannover&units=metric&appid=6065bced8e4da7753c949661f8073544
-
-// https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&units=metric&appid=6065bced8e4da7753c949661f8073544
