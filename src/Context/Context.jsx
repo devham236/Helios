44b929519e3f65a7063d653c505bbdb75352a6d6
devham236 <90037750+devham236@users.noreply.github.com>
2023-10-15 10:25:03 +0200

    clean up

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -6,24 +6,9 @@ function ContextProvider({ children }) {
   const [modalOpened, setModalOpened] = useState(false)
   const [modal, setModal] = useState(null)
   const [arrayEmpty, setArrayEmpty] = useState(false)
-  const [opened, setOpened] = useState(
-    // () => JSON.parse(localStorage.getItem("opened")) || false
-    false
-  )
-  const [weatherArray, setWeatherArray] = useState(
-    // () => JSON.parse(localStorage.getItem("weatherArray")) || []
-    []
-  )
-  const [activeCard, setActiveCard] = useState(
-    // () => JSON.parse(localStorage.getItem("activeCard")) || null
-    null
-  )
-
-  // useEffect(() => {
-  //   localStorage.setItem("activeCard", JSON.stringify(activeCard))
-  //   localStorage.setItem("opened", JSON.stringify(opened))
-  //   localStorage.setItem("weatherArray", JSON.stringify(weatherArray))
-  // }, [opened, weatherArray, activeCard])
+  const [opened, setOpened] = useState(false)
+  const [weatherArray, setWeatherArray] = useState([])
+  const [activeCard, setActiveCard] = useState(null)
 
   useEffect(() => {
     if (weatherArray.length === 0) {
