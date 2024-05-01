f46ed0f15881abb029c0603f0c44675770b131bc
Hamza Mirza <hamza665@hotmail.de>
2024-05-01 12:42:59 +0200

    sidebar reducer

---
+++ src/Slices/sidebarSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const sidebarSlice = createSlice({
+  name: "sidebar",
+  initialState: { value: {} },
+  reducers: {},
+})
+
+export default sidebarSlice.reducer
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./styles.css"
 import { ContextProvider } from "./Context/Context"
+import { configureStore } from "@reduxjs/toolkit"
+import { Provider } from "react-redux"
+import sidebarReducer from "./Slices/sidebarSlice"
+
+const store = configureStore({
+  reducer: {
+    sidebar: sidebarReducer,
+  },
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ContextProvider>
-    <React.StrictMode>
+  <Provider store={store}>
+    <ContextProvider>
       <App />
-    </React.StrictMode>
-  </ContextProvider>
+    </ContextProvider>
+  </Provider>
 )
