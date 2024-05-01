88255b0a86b1c4a87cfb8be7a7f9b023c4a51867
Hamza Mirza <hamza665@hotmail.de>
2024-05-01 11:57:47 +0200

    clean up

--- src/Utils/getTime.js
+++ src/Utils/getTime.js
@@ -1,21 +1,10 @@
 import { DateTime } from "luxon"
 
 function getTime(timestamp, offset) {
-  const dateTime = DateTime.fromSeconds(timestamp).plus({ seconds: offset });
-  const formattedTime = dateTime.toFormat('HH:mm');
+  const dateTime = DateTime.fromSeconds(timestamp).plus({ seconds: offset })
+  const formattedTime = dateTime.toFormat("HH:mm")
 
-  return formattedTime;
+  return formattedTime
 }
- 
+
 export default getTime
-
-// const date = new Date((timestamp + (offset)) * 1000)
-    // const hours = date.getHours()
-    // const minutes = date.getMinutes()
-
-    // const formattedHours = hours < 10 ? `0${hours}` : hours
-    // const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes
-
-    // const formattedTime = `${formattedHours}:${formattedMinutes}`
-
-    // return formattedTime
