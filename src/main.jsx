b51007eb5adcdeb60434539372a63aaefc51ff42
Hamza Mirza <hamza665@hotmail.de>
2024-05-20 08:59:46 +0200

    modal slice

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import { ContextProvider } from "./Context/Context"
 import { configureStore } from "@reduxjs/toolkit"
 import { Provider } from "react-redux"
 import sidebarReducer from "./Slices/sidebarSlice"
+import modalReducer from "./Slices/modalSlice"
 
 const store = configureStore({
   reducer: {
     sidebar: sidebarReducer,
+    modal: modalReducer,
   },
 })
 
